261cefb596351543bff3f80f42e04dd72024a217
HernandoVargasL <91139093+HernandoVargasL@users.noreply.github.com>
2022-07-06 15:13:21 -0500

    update recursos

--- index.js
+++ index.js
@@ -419,3 +419,20 @@ $(function () {
     $('td.estados:contains(Activo)').html('<span class="activo">Activo</span>')
 });
 
+/*tabulador*/
+if (document.querySelector(".tab-content")) {
+    var button = document.getElementsByClassName('but'),
+        tabContent = document.getElementsByClassName('tab-content');
+        tabContent[0].style.display = 'block';
+    
+    
+    function city(e, city) {
+        var i;
+        for (i = 0; i < button.length; i++) {
+            tabContent[i].style.display = 'none';
+            button[i].classList.remove('active');
+        }
+        document.getElementById(city).style.display = 'block';
+        e.currentTarget.classList.add('active');
+    }
+};
