0f286ebaed8374d45f104ab49881f80e50d24f63
homeristico <homeristico@hotmail.com>
2023-04-18 16:52:53 -0500

    agregado icono usuario al navbar y componente offcanvas

--- index.js
+++ index.js
@@ -28,7 +28,39 @@ if (document.querySelector(".navbarIgacContainer")) {
                 <a class='redes' href='/redes/red_geodesica.html' rel='noopener'>Redes</a>
               </li>
               <li>
-                <a class='recursos' href='/herramientas/servicios.html' rel='noopener'>Recursos</a>
+                <a class='recursos' href='/herramientas/servicios.html' rel='noopener'>Recursos</a>                
+              </li>
+              <li>
+                  
+                 <img id="icono-user"  src="/images/iconos/person-circle.svg"  data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"></img>
+
+              
+                
+
+                  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas-header">
+                      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Zona Usuario</h5>
+                      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                      <div class="container mt-5">
+                        <div class="row">
+                          <div class="col">
+
+                              
+
+                          </div>
+                        </div>
+                        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+                      </div>
+                      
+                    </div>
+                  </div>
+                                  
+                  
+                  
+                  
+                  
               </li>
           </ul>
           <button class='nav-bar-toggle-igac'>
